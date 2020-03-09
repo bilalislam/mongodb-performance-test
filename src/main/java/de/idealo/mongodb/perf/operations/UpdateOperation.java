269c1e8f20b4fafa058695b08f1d37169e7672ca
bislam <bilal.islam@hepsiburada.com>
2020-03-09 22:11:34 +0300

    refactored

--- src/main/java/de/idealo/mongodb/perf/operations/UpdateOperation.java
+++ src/main/java/de/idealo/mongodb/perf/operations/UpdateOperation.java
@@ -1,35 +1,35 @@
 package de.idealo.mongodb.perf.operations;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.result.UpdateResult;
 import de.idealo.mongodb.perf.MongoDbAccessor;
-import org.bson.Document;
 
-import static com.mongodb.client.model.Filters.eq;
+import java.util.UUID;
 
 /**
  * Created by kay.agahd on 23.11.16.
  */
 public class UpdateOperation extends AbstractOperation {
 
-    public UpdateOperation(MongoDbAccessor mongoDbAccessor, String db, String collection, String field){
+    public UpdateOperation(MongoDbAccessor mongoDbAccessor, String db, String collection, String field) {
         super(mongoDbAccessor, db, collection, field);
     }
 
     @Override
-    long executeQuery(int threadId, long threadRunCount, long globalRunCount, long selectorId, long randomId){
+    long executeQuery(int threadId, long threadRunCount, long globalRunCount, long selectorId, long randomId) {
+        BasicDBObject searchQuery = new BasicDBObject("Customer._id", UUID.fromString("27becf70-8ae7-4d04-8d02-f6a6ed5a76f3"));
+        searchQuery.append("Status", new BasicDBObject("Value", "Active"));
 
-        final Document doc = new Document("$set", new Document(RANDOM_LONG, randomId))
-                .append("$inc", new Document(VERSION, 1));
+        BasicDBObject updateQuery = new BasicDBObject();
+        updateQuery.append("$set", new BasicDBObject("Customer.EmailAddress", "test"));
 
-        final UpdateResult res = THREAD_RUN_COUNT.equals(queriedField)?mongoCollection.updateMany(eq(queriedField, selectorId), doc)
-                :ID.equals(queriedField)?mongoCollection.updateOne(eq(queriedField, selectorId), doc):null;
-        return res!=null?res.getModifiedCount():0l;
+        return mongoCollection.updateOne(searchQuery, updateQuery).getModifiedCount();
     }
 
     @Override
-    public OperationModes getOperationMode(){
-        if(IOperation.THREAD_RUN_COUNT.equals(queriedField)) return OperationModes.UPDATE_MANY;
+    public OperationModes getOperationMode() {
+        if (IOperation.THREAD_RUN_COUNT.equals(queriedField)) return OperationModes.UPDATE_MANY;
         else return OperationModes.UPDATE_ONE;
-    };
-
+    }
 }
